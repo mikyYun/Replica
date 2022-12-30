240c9d2ea342a8826db688a2984679b23bc67aa9
mikyYun <mkyun2714@gmail.com>
2022-12-29 20:01:47 -0800

    complete toggle menu

--- kristinLisaDesign/main.js
+++ kristinLisaDesign/main.js
@@ -1,12 +1,37 @@
+let target;
+
 function toggleClass() {
+  const menuContainer = document.getElementById("menu_container");
+  
   const menuIcon = document.getElementById("menu_icon")
   const isNotActive = menuIcon.className.includes("not-active")
-  console.log(isNotActive)
   if (isNotActive) {
     menuIcon.classList.add("active")
     menuIcon.classList.remove("not-active")
+    menuContainer.classList.add("show")
   } else {
     menuIcon.classList.add("not-active")
     menuIcon.classList.remove("active")
+    menuContainer.classList.remove("show")
   }
+
 }
+
+
+const elements = [...document.getElementsByClassName("menu_name")]
+elements.forEach(el => {
+  el.addEventListener("click", (e) => {
+    const hasSibling = e.target.nextElementSibling
+    
+    if (hasSibling) {
+      if (target == hasSibling) {
+        target.classList.remove("show")
+      } else {
+
+        target && target.classList.remove("show");
+        target = hasSibling;
+        target.classList.add("show")
+      }
+    } else return;
+  })
+})
